dd8d4ef532e66538bd62b949224078a6989de4e2
Ssxr <70914425+Ssxr@users.noreply.github.com>
2025-02-16 23:02:02 +0800

    Changes in Keyboard.java

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Keyboard.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Keyboard.java
@@ -1,21 +1,39 @@
 package io.github.some_example_name.lwjgl3;
 
 import com.badlogic.gdx.Input;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Keyboard {
     private InputOutputManager ioManager;
-
+    private final Set<Integer> pressedKeys = new HashSet<>();
+    
     public Keyboard(InputOutputManager ioManager) {
         this.ioManager = ioManager;
     }
 
+//    public void checkKeys() {
+//        int[] keys = {Input.Keys.W, Input.Keys.A, Input.Keys.S, Input.Keys.D,
+//                      Input.Keys.UP, Input.Keys.DOWN, Input.Keys.LEFT, Input.Keys.RIGHT};
+//
+//        for (int key : keys) {
+//            if (ioManager.isKeyPressed(key)) {
+//                System.out.println("Key Pressed: " + ioManager.getMappedAction(key));
+//            }
+//        }
+//    }
     public void checkKeys() {
         int[] keys = {Input.Keys.W, Input.Keys.A, Input.Keys.S, Input.Keys.D,
                       Input.Keys.UP, Input.Keys.DOWN, Input.Keys.LEFT, Input.Keys.RIGHT};
 
         for (int key : keys) {
             if (ioManager.isKeyPressed(key)) {
-                System.out.println("Key Pressed: " + ioManager.getMappedAction(key));
+                if (!pressedKeys.contains(key)) { // Log only if not already logged
+                    pressedKeys.add(key);
+                    System.out.println("Key Pressed: " + ioManager.getMappedAction(key));
+                }
+            } else {
+                pressedKeys.remove(key); // Remove when key is released
             }
         }
     }
