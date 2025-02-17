33fec9ca963b9bb5ab62feebb16a464e435eeb09
Zafri <Zafri@LAPTOP-MRVBPTIS>
2025-02-17 17:44:13 +0800

    Updated Class

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/NonCollidable.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/NonCollidable.java
@@ -1,5 +1,17 @@
 package io.github.some_example_name.lwjgl3;
 
-public class NonCollidable {
+import com.badlogic.gdx.math.Rectangle;
 
+public class NonCollidable extends Collidable {
+
+    public NonCollidable(String collisionType, Rectangle hitbox) {
+        super(collisionType, hitbox);
+        this.isSolid = false;  // Non-collidables are not solid
+    }
+
+    @Override
+    public boolean detectCollision(Collidable other) {
+        return false; // Non-collidables should not trigger collisions
+    }
 }
+
