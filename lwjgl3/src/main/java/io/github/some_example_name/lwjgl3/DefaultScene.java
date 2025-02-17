80b9868c2033fe9ed5d58b7513e83c823b502933
Wei Qin <Wei Qin@LAPTOP-J0DEBJGL>
2025-02-17 23:39:40 +0800

    Default Scene

---
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/DefaultScene.java
@@ -0,0 +1,66 @@
+package io.github.some_example_name.lwjgl3;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class DefaultScene extends Scene {
+
+	public DefaultScene(SceneManager sceneManager) {
+		super(sceneManager);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
