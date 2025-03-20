80c1f66be39efc2b0c568097bfcebadfca25e4f0
SleepyCacti <2403108@sit.singaporetech.edu.sg>
2025-03-20 21:48:52 +0800

    Collision Logic
    Collidable changed to interface and non-collidable implements
    collidable. Collisionmanager checkcollision added.

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Collidable.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Collidable.java
@@ -1,38 +1,10 @@
 package io.github.some_example_name.lwjgl3;
 
-public abstract class Collidable {
-    public boolean isSolid = true;
-    public String collisionType;   
-    private int gridX, gridY;  
-
-    public Collidable(String collisionType, int gridX, int gridY) {
-        this.collisionType = collisionType;
-        this.gridX = gridX;
-        this.gridY = gridY;
-    }
-
-    public boolean detectCollision(Collidable other) {
-        // Check if two objects occupy the same grid cell
-        return this.gridX == other.gridX && this.gridY == other.gridY;
-    }
-
-    public int getGridX() {
-        return gridX;
-    }
-
-    public int getGridY() {
-        return gridY;
-    }
-
-    public void setGridX(int gridX) {
-        this.gridX = gridX;
-    }
-
-    public void setGridY(int gridY) {
-        this.gridY = gridY;
-    }
-
-    //public void resolveCollision(Collidable other) {
-        // Default collision resolution logic (can be overridden by subclasses)
-    //}
+public interface Collidable {
+    boolean isSolid();
+    int getGridX();
+    int getGridY();
+    void setGridX(int gridX);
+    void setGridY(int gridY);
+    boolean detectCollision(Collidable other);
 }
