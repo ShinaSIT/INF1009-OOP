32d8e14d4fa81361a5a09cd5fa7fcc5da1d774c2
Olilian <2402999@sit.singaporetech.edu.sg>
2025-03-26 18:37:19 +0800

    renamed players variable name

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/CharacterAssets.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/CharacterAssets.java
@@ -4,45 +4,45 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.Gdx;
 
 public class CharacterAssets {
-    public static Texture psyduckLUp, psyduckLUp2;
-    public static Texture psyduckLDown, psyduckLDown2;
-    public static Texture psyduckLLeft, psyduckLLeft2;
-    public static Texture psyduckLRight, psyduckLRight2;
+    public static Texture playerLUp, playerLUp2;
+    public static Texture playerLDown, playerLDown2;
+    public static Texture playerLLeft, playerLLeft2;
+    public static Texture playerLRight, playerLRight2;
 
-    public static Texture psyduckRUp, psyduckRUp2;
-    public static Texture psyduckRDown, psyduckRDown2;
-    public static Texture psyduckRLeft, psyduckRLeft2;
-    public static Texture psyduckRRight, psyduckRRight2;
+    public static Texture playerRUp, playerRUp2;
+    public static Texture playerRDown, playerRDown2;
+    public static Texture playerRLeft, playerRLeft2;
+    public static Texture playerRRight, playerRRight2;
 
     public static void load() {
-        psyduckLUp = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLUp.png"));
-        psyduckLUp2 =  new Texture(Gdx.files.internal("player/Right Facing/PsyduckRDown.png"));
-        psyduckLDown = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLDown.png"));
-        psyduckLDown2 = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLUp.png"));
-        psyduckLLeft = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLLeft.png"));
-        psyduckLLeft2 = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLRight.png"));
-        psyduckLRight = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLRight.png"));
-        psyduckLRight2 = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLLeft.png"));
+    	playerLUp = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLUp.png"));
+    	playerLUp2 =  new Texture(Gdx.files.internal("player/Right Facing/PsyduckRDown.png"));
+    	playerLDown = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLDown.png"));
+    	playerLDown2 = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLUp.png"));
+    	playerLLeft = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLLeft.png"));
+    	playerLLeft2 = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLRight.png"));
+    	playerLRight = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLRight.png"));
+    	playerLRight2 = new Texture(Gdx.files.internal("player/Left Facing/PsyduckLLeft.png"));
 
-        psyduckRUp = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRUp.png"));
-        psyduckRUp2 = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRDown.png"));
-        psyduckRDown = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRDown.png"));
-        psyduckRDown2 = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRUp.png"));
-        psyduckRLeft = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRLeft.png"));
-        psyduckRLeft2 = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRRight.png"));
-        psyduckRRight = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRRight.png"));
-        psyduckRRight2 = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRLeft.png"));
+    	playerRUp = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRUp.png"));
+    	playerRUp2 = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRDown.png"));
+    	playerRDown = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRDown.png"));
+    	playerRDown2 = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRUp.png"));
+    	playerRLeft = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRLeft.png"));
+    	playerRLeft2 = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRRight.png"));
+    	playerRRight = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRRight.png"));
+    	playerRRight2 = new Texture(Gdx.files.internal("player/Right Facing/PsyduckRLeft.png"));
     }
 
     public static void dispose() {
-        psyduckLUp.dispose(); psyduckLUp2.dispose();
-        psyduckLDown.dispose(); psyduckLDown2.dispose();
-        psyduckLLeft.dispose(); psyduckLLeft2.dispose();
-        psyduckLRight.dispose(); psyduckLRight2.dispose();
+    	playerLUp.dispose(); playerLUp2.dispose();
+    	playerLDown.dispose(); playerLDown2.dispose();
+        playerLLeft.dispose(); playerLLeft2.dispose();
+        playerLRight.dispose(); playerLRight2.dispose();
 
-        psyduckRUp.dispose(); psyduckRUp2.dispose();
-        psyduckRDown.dispose(); psyduckRDown2.dispose();
-        psyduckRLeft.dispose(); psyduckRLeft2.dispose();
-        psyduckRRight.dispose(); psyduckRRight2.dispose();
+        playerRUp.dispose(); playerRUp2.dispose();
+        playerRDown.dispose(); playerRDown2.dispose();
+        playerRLeft.dispose(); playerRLeft2.dispose();
+        playerRRight.dispose(); playerRRight2.dispose();
     }
 }
